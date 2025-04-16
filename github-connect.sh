fe8a70bfe048dcb30f7d0ebb0b87111dab079dfb
Komarov Dmitriy <d.komarov@pyn.ru>
2025-04-16 11:35:02 +0300

    beaty scripts

--- github-connect.sh
+++ github-connect.sh
@@ -1,14 +1,31 @@
+#!/bin/bash
+
+domain="${1:-email.ru}"
+
 current_username=$(id -un)
-echo "Текущий пользователь $current_username"
+echo "Текущий пользователь: $current_username"
+
 git config --global user.name "$current_username"
 
-current_email=$((current_username + '@email.ru'))
-echo "Текущий emil $current_email"
+current_email="${current_username}@${domain}"
+echo "Текущий email: $current_email"
+
 git config --global user.email "$current_email"
 
-echo 'Генерируем ключ в интерактивном режиме для github.com '
+echo "Генерируем ключ в интерактивном режиме для github.com"
 ssh-keygen -t rsa
-echo 'Ключ сгенерирован ---> '
+
+echo "Ключ сгенерирован --->"
 echo
 cat ~/.ssh/id_rsa.pub
-echo 'Что бы добавить ключ перейдите по https://github.com/settings/ssh/new '
+
+urls=(
+  "https://github.com/settings/ssh/new"
+  "https://gitlab.com/-/user_settings/ssh_keys"
+  ""
+)
+
+echo "Чтобы добавить ключ, перейдите по одной из ссылок:"
+for url in "${urls[@]}"; do
+  echo "$url"
+done
--- init-env.sh
+++ init-env.sh
@@ -1,8 +1,10 @@
 echo 'Проверяем, установлен ли Homebrew'
 if ! command -v brew &> /dev/null
 then
-    echo 'Устанавливаем Homebrew'
+    echo 'Устанавливаем Homebrew...'
+    echo
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 else
     echo 'Homebrew уже установлен'
 fi
